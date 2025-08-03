ae667f7a1b4c2caaa829940e43eb1749ee55566d
azhar1598 <mohammedazhar.1598@gmail.com>
2025-08-03 21:22:35 +0530

    [FIX] PDF

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000", // Local dev
-  "https://tulboxx.vercel.app", // Deployed frontend
+  // "https://tulboxx.vercel.app", // Deployed frontend
   "https://app.tulboxx.com",
   "https://tulboxx.com",
 ];
